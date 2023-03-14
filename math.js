bc71b32b73d753b5a57da8c1ca0a92a1d723c692
HuldaCZ <phulinsky@gmail.com>
2023-03-14 22:43:43 +0100

    🚧 Chart setup

--- math.js
+++ math.js
@@ -3,3 +3,15 @@ const math = {}
 math.lerp = (a, b, t) => {
   return a + (b - a) * t
 }
+
+math.invLerp = (a, b, v) => {
+  return (v - a) / (b - a)
+}
+
+math.remap = (oldA, oldB, newA, newB, v) => {
+  return math.lerp(newA, newB, math.invLerp(oldA, oldB, v))
+}
+
+math.formatNumber = (n, dec=0) => {
+  return n.toFixed(dec)
+}
